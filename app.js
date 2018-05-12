6f89d6cd80d3a782570ad3687cd5579b1d59d68c
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-05-12 16:07:14 -0500

    end points working

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ app.get('/add/:param1/:param2', function (req, res, next) {
     let sum = +req.params.param1 + +req.params.param2
     res.send(`${sum}`)
 })
+
+app.get('/add', function(req, res, next) {
+    const sum = +req.query.param1 + +req.query.param2
+    res.send(`${sum}`)
+})
+
+app.use('/substract/:param1/:param2', function(req, res, next) {
+    let substraction = +req.params.param1 - +req.params.param2
+    res.send(`${substraction}`)
+})
+
+app.use('/substract', function(req, res, next) {
+    let substraction = +req.query.param1 - +req.query.param2
+    res.send(`${substraction}`)
+})
+
+app.use('/multiply/:param1/:param2', function (req,res,next) {
+    let multiplication = +req.params.param1 * +req.params.param2
+    res.send(`${multiplication}`)
+})
+
+app.use('/multiply', function (req,res,next) { 
+    let multiplication = +req.query.param1 * +req.query.param2
+    res.send(`${multiplication}`)
+})
+
+app.use('/divide/:param1/:param2', function (req,res, next) {
+    let division = +req.params.param1 / +req.params.param2
+    res.send(`${division}`)
+})
+
+app.use('/divide', function (req, res, next) {
+    let division = +req.query.param1 / +req.query.param2
+    res.send(`${division}`)
+})
