177380aae09665d5726e935ac03403ee1f65630f
aurre <aurrecochearaysa@gmail.com>
2018-05-13 15:01:08 -0500

    Ui finished

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-'use strict'
+'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-const volleyball = require('volleyball')
+const bodyParser = require('body-parser');
+const volleyball = require('volleyball');
 
 app.use(volleyball);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
-app.get('/status', function (req, res, next) {
-    res.send({'Status': 'Ok'})
-})
+app.get('/status', function(req, res, next) {
+  res.send({ Status: 'Ok' });
+});
 
-app.get('/add/:param1/:param2', function (req, res, next) {
-    let sum = +req.params.param1 + +req.params.param2
-    res.send(`${sum}`)
-})
+app.get('/add/:num1/:num2', function(req, res, next) {
+  let sum = +req.params.num1 + +req.params.num2;
+  res.send(`${sum}`);
+});
 
 app.get('/add', function(req, res, next) {
-    const sum = +req.query.param1 + +req.query.param2
-    res.send(`${sum}`)
-})
+  const sum = +req.query.num1 + +req.query.num2;
+  res.send(`${sum}`);
+});
 
-app.use('/substract/:param1/:param2', function(req, res, next) {
-    let substraction = +req.params.param1 - +req.params.param2
-    res.send(`${substraction}`)
-})
+app.use('/substract/:num1/:num2', function(req, res, next) {
+  let substraction = +req.params.num1 - +req.params.num2;
+  res.send(`${substraction}`);
+});
 
 app.use('/substract', function(req, res, next) {
-    let substraction = +req.query.param1 - +req.query.param2
-    res.send(`${substraction}`)
-})
+  let substraction = +req.query.num1 - +req.query.num2;
+  res.send(`${substraction}`);
+});
 
-app.use('/multiply/:param1/:param2', function (req,res,next) {
-    let multiplication = +req.params.param1 * +req.params.param2
-    res.send(`${multiplication}`)
-})
+app.use('/multiply/:num1/:num2', function(req, res, next) {
+  let multiplication = +req.params.num1 * +req.params.num2;
+  res.send(`${multiplication}`);
+});
 
-app.use('/multiply', function (req,res,next) { 
-    let multiplication = +req.query.param1 * +req.query.param2
-    res.send(`${multiplication}`)
-})
+app.use('/multiply', function(req, res, next) {
+  let multiplication = +req.query.num1 * +req.query.num2;
+  res.send(`${multiplication}`);
+});
 
-app.use('/divide/:param1/:param2', function (req,res, next) {
-    let division = +req.params.param1 / +req.params.param2
-    res.send(`${division}`)
-})
+app.use('/divide/:num1/:num2', function(req, res, next) {
+  let division = +req.params.num1 / +req.params.num2;
+  res.send(`${division}`);
+});
 
-app.use('/divide', function (req, res, next) {
-    let division = +req.query.param1 / +req.query.param2
-    res.send(`${division}`)
-})
+app.use('/divide', function(req, res, next) {
+  let division = +req.query.num1 / +req.query.num2;
+  res.send(`${division}`);
+});
